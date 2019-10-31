4782da19b860724df035f8c7614776e5cdcc0c42
darceym <darcey.meus@gmail.com>
2019-10-31 09:57:32 -0400

    added the value of the password to the text box to display

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function firstClick() {
-    document.getElementById("btnGen").index.HTML =
-    prompt("How long would you like your password? Please pick between 8 - 128.");
-}
-function firstClick() {
-    prompt("What length would you like your password? Please pick between 8 - 128?");
+// function firstClick() {
+//     document.getElementById("btnGen").index.HTML =
+//     prompt("How long would you like your password? Please pick between 8 - 128.");
+// }
+// function firstClick() {
+//     prompt("What length would you like your password? Please pick between 8 - 128?");
     
-   }
+//    }
